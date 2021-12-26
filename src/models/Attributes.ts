900b9acc30688a75f8fe490086572cf381e26330
Luigi Garcia <luigifgp@gmail.com>
2021-12-26 19:01:54 +0000

    Initial commit

---
+++ src/models/Attributes.ts
@@ -0,0 +1,18 @@
+
+
+export class Attributes<T> {
+
+  constructor(public data: T){}  
+
+  get = <K extends keyof T>(key: K): T[K] => {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+
+  getAll(): T {
+    return this.data
+  }
+}
